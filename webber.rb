8f462144096545529e1ed6a47964db4eea525a57
iMike <isaev.mihael@gmail.com>
2021-03-04 00:03:49 +0400

    Add `npm` dependency

--- webber.rb
+++ webber.rb
@@ -8,7 +8,8 @@ class Webber < Formula
     url "https://github.com/swifweb/webber.git", tag: "0.2.0", revision: "2c02e9e3fcde3f6d29f28c2c2289e0e487e7c723"
     sha256 "fbe25682215fcd5b42f4f4cdbd86de046427310c27af19aaaaeeeec69956baec"
   end
-
+  
+  depends_on "npm"
   depends_on "binaryen"
   depends_on "wasmer"
   depends_on :xcode => "11.4"
