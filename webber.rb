93563f15bca5aeceb1fb330ac9d04c1e0ac2636b
iMike <isaev.mihael@gmail.com>
2021-03-05 05:07:10 +0400

    Bump version to 0.3.3

--- webber.rb
+++ webber.rb
@@ -4,9 +4,9 @@ class Webber < Formula
   head "https://github.com/swifweb/webber.git"
 
   stable do
-    version "0.3.2"
-    url "https://github.com/swifweb/webber.git", tag: "0.3.2", revision: "4fb1d430ca945b270f47597d3454607d88875350"
-    sha256 "f77e0ca4601bbc2b33279e4965080c150ab051b4787d39b3ea0cee0f10f6eaf1"
+    version "0.3.3"
+    url "https://github.com/swifweb/webber.git", tag: "0.3.3", revision: "5745c042e66b9f0d2059063ab6926bd3941382bc"
+    sha256 "38ea0e8a6ee732724ee1caddf0648132680977391c4093056039570906ad267a"
   end
   
   depends_on "binaryen"
