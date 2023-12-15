e2baf5dde61c5d9dee2ec494cf2cde1f32ff01bd
Mikhail Isaev aka iMike <isaev.mihael@gmail.com>
2023-12-15 04:57:03 +0400

    Bump version to 1.7.1

--- webber.rb
+++ webber.rb
@@ -4,9 +4,9 @@ class Webber < Formula
   head "https://github.com/swifweb/webber.git"
 
   stable do
-    version "1.7.0"
-    url "https://github.com/swifweb/webber.git", tag: "1.7.0", revision: "06302df3156a673cf50d8f0be5ba48739fb5785b"
-    sha256 "aed327d75ad74423ee69fab97be2793cdc6a583f81fc01a47a6cd9d1c97bf370"
+    version "1.7.1"
+    url "https://github.com/swifweb/webber.git", tag: "1.7.1", revision: "c9fa362ba12f5d5d5bc7d44627dc2e8e49c991c6"
+    sha256 "8d8a16cdf7cdf5bcf1001e0bca73f90965c4ac09fcffebb40255d5e4fc70a45a"
   end
   
   depends_on "binaryen"
